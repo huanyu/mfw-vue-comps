771748e6e7728617f33eaf1a2124dde3c3b57e8f
git <git@git.com>
2019-04-28 11:24:11 +0800

    upgrade project with vue cli 3

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by hb on 2019/4/28.
+ */
+
+import CDLabel from './cdLabel'
+
+// export CDLabel
+
+const Componets = {CDLabel}
+
+// Object.keys(Componets.forEach(name => {
+//   Vue.component(name, Componets[name])
+// }))
+
+export default Componets
